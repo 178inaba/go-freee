440fc448dc047c68d0e3afc5d30fb5e1106045c3
178inaba <178inaba@users.noreply.github.com>
2019-10-18 00:37:33 +0900

    Fix NewClient

--- freee.go
+++ freee.go
@@ -1,10 +1,32 @@
 package freee
 
+import (
+	"net/http"
+	"net/url"
+)
+
+const defaultBaseRawurl = "https://api.freee.co.jp/"
+
 // Client is freee client.
 type Client struct {
+	BaseURL *url.URL
+
+	client *http.Client
 }
 
 // NewClient returns freee client.
-func NewClient() *Client {
-	return &Client{}
+func NewClient(httpClient *http.Client) (*Client, error) {
+	if httpClient == nil {
+		httpClient = &http.Client{}
+	}
+
+	u, err := url.Parse(defaultBaseRawurl)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		client:  httpClient,
+		BaseURL: u,
+	}, nil
 }
