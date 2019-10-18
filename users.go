c5efc9242273edc75a7320e010c1ed77a9cb3e9a
178inaba <178inaba@users.noreply.github.com>
2019-10-19 04:13:39 +0900

    Add UserCapability

--- users.go
+++ users.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"net/url"
+	"strconv"
 )
 
 // User is user.
@@ -28,16 +29,109 @@ type UserCompany struct {
 
 // User returns login user.
 func (c *Client) User(ctx context.Context) (*User, error) {
+	q := url.Values{}
+	q.Set("companies", "true")
+
 	var u struct {
 		User `json:"user"`
 	}
-
-	q := url.Values{}
-	q.Set("companies", "true")
 
 	if err := c.do(ctx, http.MethodGet, "users/me", q, &u); err != nil {
 		return nil, err
 	}
 
 	return &u.User, nil
+}
+
+// UserCapability is user capability.
+type UserCapability struct {
+	WalletTxns                   Capability `json:"wallet_txns"`
+	Deals                        Capability `json:"deals"`
+	Transfers                    Capability `json:"transfers"`
+	Docs                         Capability `json:"docs"`
+	DocPostings                  Capability `json:"doc_postings"`
+	Receipts                     Capability `json:"receipts"`
+	ReceiptStreamEditor          Capability `json:"receipt_stream_editor"`
+	ExpenseApplications          Capability `json:"expense_applications"`
+	Spreadsheets                 Capability `json:"spreadsheets"`
+	PaymentRequests              Capability `json:"payment_requests"`
+	RequestForms                 Capability `json:"request_forms"`
+	ApprovalRequests             Capability `json:"approval_requests"`
+	Reports                      Capability `json:"reports"`
+	ReportsIncomeExpense         Capability `json:"reports_income_expense"`
+	ReportsReceivables           Capability `json:"reports_receivables"`
+	ReportsPayables              Capability `json:"reports_payables"`
+	ReportsCash_balance          Capability `json:"reports_cash_balance"`
+	ReportsCrosstabs             Capability `json:"reports_crosstabs"`
+	ReportsGeneralLedgers        Capability `json:"reports_general_ledgers"`
+	ReportsPl                    Capability `json:"reports_pl"`
+	ReportsBs                    Capability `json:"reports_bs"`
+	ReportsJournals              Capability `json:"reports_journals"`
+	ReportsManagementsPlanning   Capability `json:"reports_managements_planning"`
+	ReportsManagementsNavigation Capability `json:"reports_managements_navigation"`
+	ManualJournals               Capability `json:"manual_journals"`
+	FixedAssets                  Capability `json:"fixed_assets"`
+	InventoryRefreshes           Capability `json:"inventory_refreshes"`
+	BizAllocations               Capability `json:"biz_allocations"`
+	PaymentRecords               Capability `json:"payment_records"`
+	AnnualReports                Capability `json:"annual_reports"`
+	TaxReports                   Capability `json:"tax_reports"`
+	ConsumptionEntries           Capability `json:"consumption_entries"`
+	TaxReturn                    Capability `json:"tax_return"`
+	AccountItemStatements        Capability `json:"account_item_statements"`
+	MonthEnd                     Capability `json:"month_end"`
+	YearEnd                      Capability `json:"year_end"`
+	Walletables                  Capability `json:"walletables"`
+	Companies                    Capability `json:"companies"`
+	Invitations                  Capability `json:"invitations"`
+	SignInLogs                   Capability `json:"sign_in_logs"`
+	Backups                      Capability `json:"backups"`
+	OpeningBalances              Capability `json:"opening_balances"`
+	SystemConversion             Capability `json:"system_conversion"`
+	Resets                       Capability `json:"resets"`
+	Partners                     Capability `json:"partners"`
+	Items                        Capability `json:"items"`
+	Sections                     Capability `json:"sections"`
+	Tags                         Capability `json:"tags"`
+	AccountItems                 Capability `json:"account_items"`
+	Taxes                        Capability `json:"taxes"`
+	PayrollItemSets              Capability `json:"payroll_item_sets"`
+	UserMatchers                 Capability `json:"user_matchers"`
+	DealTemplates                Capability `json:"deal_templates"`
+	ManualJournalTemplates       Capability `json:"manual_journal_templates"`
+	CostAllocations              Capability `json:"cost_allocations"`
+	ApprovalFlowRoutes           Capability `json:"approval_flow_routes"`
+	ExpenseApplicationTemplates  Capability `json:"expense_application_templates"`
+	Workflows                    Capability `json:"workflows"`
+	OauthApplications            Capability `json:"oauth_applications"`
+	OauthAuthorizations          Capability `json:"oauth_authorizations"`
+	DivisionTag1                 Capability `json:"division_tag_1"`
+	DivisionTag2                 Capability `json:"division_tag_2"`
+	DivisionTag3                 Capability `json:"division_tag_3"`
+	BankAccountantStaffUsers     Capability `json:"bank_accountant_staff_users"`
+	WorkflowValidationSettings   Capability `json:"workflow_validation_settings"`
+}
+
+// Capability is capability.
+type Capability struct {
+	Confirm       bool   `json:"confirm"`
+	Read          bool   `json:"read"`
+	Create        bool   `json:"create"`
+	Update        bool   `json:"update"`
+	Destroy       bool   `json:"destroy"`
+	Sync          bool   `json:"sync"`
+	AllowedTarget string `json:"allowed_target"`
+}
+
+// UserCapability returns login user capability.
+func (c *Client) UserCapability(ctx context.Context, companyID int) (*UserCapability, error) {
+	q := url.Values{}
+	q.Set("company_id", strconv.Itoa(companyID))
+
+	var uc UserCapability
+	if err := c.do(ctx, http.MethodGet, "users/capabilities", q, &uc); err != nil {
+		return nil, err
+	}
+
+	return &uc, nil
 }
